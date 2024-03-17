1d9b2bdb3b1f7d72904e09304f95f2ca58230742
pkusunjy <767978963@qq.com>
2024-03-17 11:14:52 +0800

    invoke liboai test

--- test/test_liboai.cpp
+++ test/test_liboai.cpp
@@ -6,3 +6,23 @@ TEST(liboai, add) {
     ASSERT_EQ(4, 2 + 2);
 }
 
+TEST(liboai, chat_completion) {
+    liboai::OpenAI oai;
+    liboai::Conversation convo;
+    convo.AddUserData("What is the point of taxes?");
+    ASSERT_TRUE(oai.auth.SetKeyEnv("OPENAI_API_KEY"));
+
+    if (oai.auth.SetKeyEnv("OPENAI_API_KEY")) {
+        try {
+            liboai::Response response = oai.ChatCompletion->create(
+                "gpt-3.5-turbo", convo
+            );
+            convo.Update(response);
+            EXPECT_EQ("12", convo.GetLastResponse());
+            std::cout << convo.GetLastResponse() << std::endl;
+        }
+        catch (std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+}
--- test/test_util.cpp
+++ test/test_util.cpp
@@ -2,8 +2,6 @@
 #include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 
-#include "util/exception.h"
-
 TEST(util, nlohmann_json) {
     nlohmann::json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
     ASSERT_TRUE(j["happy"]);
